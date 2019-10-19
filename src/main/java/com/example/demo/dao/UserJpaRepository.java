eae283afffbd8511aa744247a04dffcff271b201
Thet Khine <mrthetkhine@gmail.com>
2019-10-19 16:38:56 +0630

    Add UserJPA Repository

---
+++ src/main/java/com/example/demo/dao/UserJpaRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.User;;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<User, Long>{
+	  
+}
