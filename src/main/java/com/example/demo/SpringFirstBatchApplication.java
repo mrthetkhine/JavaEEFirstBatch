925bd4aa504b43ab444006e13b14ca78818a5165
Thet Khine <mrthetkhine@gmail.com>
2019-11-23 16:29:48 +0630

    Update

--- src/main/java/com/example/demo/SpringFirstBatchApplication.java
+++ src/main/java/com/example/demo/SpringFirstBatchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+ 
 @SpringBootApplication
 public class SpringFirstBatchApplication {
 
