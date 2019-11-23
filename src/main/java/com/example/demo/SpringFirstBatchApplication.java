a9684c816867ff226aa105492f212fd4f082ca75
Thet Khine <mrthetkhine@gmail.com>
2019-11-23 16:11:12 +0630

    Add configuration for hibernate

--- src/main/java/com/example/demo/SpringFirstBatchApplication.java
+++ src/main/java/com/example/demo/SpringFirstBatchApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class SpringFirstBatchApplication {
 
 	public static void main(String[] args) {
